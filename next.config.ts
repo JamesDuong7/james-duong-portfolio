5b40c6b7fcdcf3d0efaed62546a33dcd59a75d60
JamesDuong7 <duongjames7@gmail.com>
2026-04-16 20:47:17 -0700

    chore: add next-sanity, @sanity/vision and allow cdn.sanity.io images

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const withBundleAnalyzer = bundleAnalyzer({
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
 };
 
 export default withBundleAnalyzer(nextConfig);
